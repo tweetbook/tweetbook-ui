a493b67bde8ed3012afd1f28f2b0e31c497d4a37
ponsea <puodsea@gmail.com>
2019-06-16 19:44:55 +0900

    TimelineModule追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { TimelineModule } from './timeline/timeline.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    TimelineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
