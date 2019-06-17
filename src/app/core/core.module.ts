c7e6c59b42fce1150e912fcbf1994b0daf383a85
takumi.ike <takumi.ike@nextbeat.net>
2019-06-17 18:32:45 +0900

    HeaderComponent場所変更

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 
 import {CoreRoutingModule} from './core-routing.module';
-import {HeaderComponent} from './presentational/header/header.component';
+import {HeaderComponent} from './header/header.component';
 import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
